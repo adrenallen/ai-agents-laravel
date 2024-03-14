3fc5d7f70975625a7d175bedd627b8c0a4972279
Garrett <garrettcoleallen@gmail.com>
2024-03-13 22:35:30 -0400

    anthropic support

---
+++ config/anthropic.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'api_key' => env('ANTHROPIC_KEY')
+];
--- src/AiAgentsLaravelServiceProvider.php
+++ src/AiAgentsLaravelServiceProvider.php
@@ -9,6 +9,7 @@ class AiAgentsLaravelServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->publishes([
+            __DIR__.'/../config/anthropic.php' => config_path('anthropic.php'),
             __DIR__.'/../config/azure_openai.php' => config_path('azure_openai.php'),
             __DIR__.'/../config/openai.php' => config_path('openai.php'),
             __DIR__.'/../config/openweathermap.php' => config_path('openweathermap.php'),
